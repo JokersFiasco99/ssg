25cbf2b3b2fc9ce4c2ee173b51d1d920ecaeaf8e
JokersFiasco99 <sozinho.gil@gmail.com>
2024-09-20 10:40:07 +0200

    B: added first code logic and unit tests

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    dummy = TextNode("This is a dummy text", "bold", "https://www.google.com")
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
